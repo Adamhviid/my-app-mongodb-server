dd07b6744eb114ce1e8a981a0845854f9aaa09e5
Adam <adam5596@stud.kea.dk>
2022-11-07 21:58:21 +0100

    register user works

--- mongodb/models/user.js
+++ mongodb/models/user.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  id: { type: String, required: true },
   email: { type: String },
   password: { type: String },
   token: { type: String },
